5c16db99929dbfde0514436432efa778af1058b4
ndg133 <38847903+ndg133@users.noreply.github.com>
2022-01-16 12:06:45 +1100

    Create index.php

---
+++ index.php
@@ -0,0 +1,48 @@
+<!-- <?php header('Access-Control-Allow-Origin: *'); ?> -->
+<?php header('Access-Control-Allow-Origin: https://taneo.nc/'); ?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<title>Tanéo API Privée</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<body>
+
+	<h1>Tanéo API Privée</h1>
+
+	<section>
+		<div>
+			<span class="name">Prochaine départ 5 Lapita vers ville</span>
+			<span class="time first"></span>
+		</div>
+
+		<div>
+			<span class="name">Prochaine départ 5 Galaxie vers ville</span>
+			<span class="time second"></span>
+		</div>
+
+		<div>
+			<span class="name">Prochaine départ 5 Galaxie vers chez papa</span>
+			<span class="time troisieme"></span>
+		</div>
+
+		<div>
+			<span class="name">Prochaine départ 3 lycée vers ville</span>
+			<span class="time quatrieme"></span>
+		</div>
+
+		<div>
+			<span class="name">Prochaine départ 6 lycée vers montravel</span>
+			<span class="time cinquieme"></span>
+		</div>
+	</section>
+
+	<section class="second"></section>
+
+
+	<script src="scripts/main.js"></script>
+
+</body>
+</html>
